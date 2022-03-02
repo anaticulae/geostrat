81cdab5372e5c66ecf3edc2e044ab1d0cb10dea5
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-03-02 12:36:44 +0100

    test(column): add no column regression test

---
+++ tests/test_no_column.py
@@ -0,0 +1,26 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+import pytest
+import serializeraw
+
+import geostrat
+
+
+@pytest.mark.parametrize('data_adjust', [True, False])
+def test_no_book200(data_adjust):
+    source = power.link(power.BOOK200_PDF)
+    ptns = serializeraw.ptn_frompath(source)
+    for navigator in ptns:
+        parsed = geostrat.parse(
+            navigator,
+            data_adjust=data_adjust,
+        )
+        assert not parsed, str(navigator.page)
