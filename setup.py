ff270d8f436bdcdf13b205d9dca2fc8e3e5c4793
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-10 20:34:08 +0000

    chore(setup): use quick install

--- setup.py
+++ setup.py
@@ -8,42 +8,11 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'geostrat/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES=[
+    'geostrat',
+]
 
 if __name__ == "__main__":
-    # allow ssetup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='give me geo',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='geostrat',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/geostrat',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'geostrat',
-        ],
-    )
+    utila.install(__file__)
